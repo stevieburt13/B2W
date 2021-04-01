ad55b3d829a4ec4dbfd58d4c736e5482f6c80537
Stevie Burton <StevieB@MacBook-Pro-4.local>
2021-04-01 18:34:00 +0100

    new pc

--- B2W/bswordpress/wp-content/themes/bootstrap2wordpress_theme/header.php
+++ B2W/bswordpress/wp-content/themes/bootstrap2wordpress_theme/header.php
@@ -25,6 +25,7 @@
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'bootstrap2wordpress' ); ?></a>
 
+
 	<header id="masthead" class="site-header">
 		<div class="site-branding">
 			<?php
